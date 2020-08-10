1a9df0607258d337360b365a6ce75a6cb7c9970e
nbrmci <cr3886165@gmail.com>
2020-08-10 17:56:20 +0900

    edit file

--- db/migrate/20200810015850_create_purchase_statuses.rb
+++ db/migrate/20200810015850_create_purchase_statuses.rb
@@ -1,8 +1,8 @@
 class CreatePurchaseStatuses < ActiveRecord::Migration[6.0]
   def change
     create_table :purchase_statuses do |t|
-      t.integer :user_id, null: false, foreign_key: true
-      t.integer :item_id, null: false, foreign_key: true
+      t.integer :user_id, foreign_key: true
+      t.integer :item_id,  foreign_key: true
       t.timestamps
     end
   end
