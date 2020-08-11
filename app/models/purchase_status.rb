0d8f23bf2a58637b7caab02f95a8fccbc483eb74
nbrmci <cr3886165@gmail.com>
2020-08-11 20:17:41 +0900

    edit file

--- app/models/purchase_status.rb
+++ app/models/purchase_status.rb
@@ -1,8 +1,8 @@
 class PurchaseStatus < ApplicationRecord
 
-    # self.data = [
-    #   { id: 1, name: '完売' }
-    # ]
+    self.data = [
+      { id: 1, name: '完売' }
+    ]
 
   with_options presence: true do
     validates :user_id
