b04020bae1082f4b5d75b425ba0845c88c1c219e
nbrmci <cr3886165@gmail.com>
2020-08-12 14:51:07 +0900

    gem payjp導入、ordersコントローラー作成、購入画面までのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  
-  # devise_scope :user do
-  #   post 'users/:id', to: 'users/registrations#new'
-  # end
 
   root to: 'items#index'
   resources :items
+  resources :orders, only: [:index, :create]
 
 end
