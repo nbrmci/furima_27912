d3e563c33292586ac6be6e585e46c76df6af5e35
nbrmci <cr3886165@gmail.com>
2020-08-18 16:40:29 +0900

    正規表現を変数に代入

--- app/forms/credit_address.rb
+++ app/forms/credit_address.rb
@@ -4,14 +4,17 @@ class CreditAddress
   attr_accessor :postal_code, :prefecture, :city, :address, :building_name, :phone_number, :user_id, :item_id, :token, :price
 
   with_options presence: true do
-    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
-    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    7_digits_with_hyphen = /\A\d{3}[-]\d{4}\z/
+    validates :postal_code, format: { with: 7_digits_with_hyphen }
+    zenkaku_japanese = /\A[ぁ-んァ-ン一-龥]/
+    validates :city, format: { with: zenkaku_japanese }
     validates :address
   end
 
   with_options numericality: { only_integer: true } do
     validates :prefecture
-    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    number_within_11_digits = /\A\d{10,11}\z/
+    validates :phone_number, format: { with: number_within_11_digits }
   end
 
   def save
