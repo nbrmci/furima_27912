e35a3990ca86a552064b72e93460a36c3f78f175
nbrmci <cr3886165@gmail.com>
2020-08-20 08:50:16 +0900

    edit file

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,6 +14,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
+    validates :image
     validates :name
     validates :describe
     validates :category_id
